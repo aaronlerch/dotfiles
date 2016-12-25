e7016defb2256161d2fbb0267082930d9d2b7114
Aaron Lerch <aaronlerch@gmail.com>
2016-12-25 13:25:06 -0600

    Function for running a python docker container

--- zshcustom/functions.zsh
+++ zshcustom/functions.zsh
@@ -21,3 +21,7 @@ function docknode() {
 function docknodebash() {
   docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] -v "$PWD":/usr/src/app -w /usr/src/app -p "3000:3000" node:6 /bin/bash "$@"
 }
+
+function dockpy() {
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] -v "$PWD":/usr/src/app -w /usr/src/app python:2.7 "$@"
+}
