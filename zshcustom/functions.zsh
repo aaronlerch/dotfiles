7979e1d9b7f671e0ea0e0e8f192fe447023ed6cd
Aaron Lerch <aaronlerch@gmail.com>
2016-12-25 00:41:04 -0600

    Randomize (a little) docker image names used for temporary images

--- zshcustom/functions.zsh
+++ zshcustom/functions.zsh
@@ -3,21 +3,21 @@ function dockbuild() {
 }
 
 function godock() {
-  docker run -it --rm --name my-app-tmp-run $1
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] "$@"
 }
 
 function dockbash() {
-  docker run -it --rm --name my-app-tmp-run $1 /bin/bash
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] $1 /bin/bash
 }
 
 function docknpm() {
-  docker run -it --rm --name my-app-tmp-run -v "$PWD":/usr/src/app -w /usr/src/app node:6 npm "$@"
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] -v "$PWD":/usr/src/app -w /usr/src/app node:6 npm "$@"
 }
 
 function docknode() {
-  docker run -it --rm --name my-app-tmp-run -v "$PWD":/usr/src/app -w /usr/src/app -p "3000:3000" node:6 node "$@"
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] -v "$PWD":/usr/src/app -w /usr/src/app -p "3000:3000" node:6 node "$@"
 }
 
 function docknodebash() {
-  docker run -it --rm --name my-app-tmp-run -v "$PWD":/usr/src/app -w /usr/src/app -p "3000:3000" node:6 /bin/bash "$@"
+  docker run -it --rm --name tmp-docker-app-$[${RANDOM}%1000] -v "$PWD":/usr/src/app -w /usr/src/app -p "3000:3000" node:6 /bin/bash "$@"
 }
