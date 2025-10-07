996f0638ce57647fdc488df2c2be8ac4cd6531b8
Aaron Lerch <aaronlerch@gmail.com>
2025-10-07 12:48:16 -0600

    Update aliases for both bash and zsh properly

--- zshcustom/aliases.zsh
+++ zshcustom/aliases.zsh
@@ -1,5 +1,4 @@
 alias ll="ls -al"
-alias subl="subl3"
 alias agi="ag -i"
 alias creep="say -v whisper"
 alias blink1="blink1-tool"
@@ -23,3 +22,8 @@ alias dococop="docorwb rubocop"
 alias docolog="doco logs --tail=100 -f"
 alias docoblog="docolog web"
 alias docodebug="docker-compose run --rm --service-ports web bundle exec rdebug-ide --port 1234 --host 0.0.0.0 --dispatcher-port 26162 -- bin/rails server -b 0.0.0.0 -p 3000"
+
+alias yarn='newt exec yarn'
+alias npm='newt exec npm'
+
+alias gam7='$HOME/bin/gam7/gam'
